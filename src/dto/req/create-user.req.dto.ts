c44eb17056fb02d233ff353f5569bedb9dc7211c
a g <axel.gallic@lyceeastier.com>
2024-02-17 14:52:14 +0000

    retrait de la partie PAS front

--- src/dto/req/create-user.req.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreateUserRequestDto {
-  @IsString()
-  readonly firstName: string;
-
-  @IsString()
-  readonly lastName: string;
-}
--- src/users/mock/user.repository.mock.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from '../../entities/user.entity';
-
-export const userRepositoryMock = {
-  save: jest.fn(),
-} as unknown as Repository<User>;
